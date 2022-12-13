6927a162ec037a2c7df8f3ff3f7d2020956bc414
kdicari24@gmail.com <kdicari24@gmail.com>
2022-12-13 10:29:59 -0500

    added Search Results and utils with Api search

--- beerme/src/components/Home.js
+++ beerme/src/components/Home.js
@@ -2,6 +2,7 @@ import "../styles/app.css"
 
 function Home() {
     // api function calls here maybe or in seperate folder?? Im thinking seperate folder
+    
 
     return (
         <div>
---
+++ beerme/src/components/SearchResultContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchResultsContainer = () => {
+    // search stuff
+}
+
+export default SearchResultsContainer;
