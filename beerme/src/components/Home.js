8906e6082f8e206f46c423c1bc97ed08bea1f086
Nicholas Bales <nickb96@icloud.com>
2022-12-13 12:08:17 -0500

    added working fetch call with tested parameters

--- beerme/src/components/Home.js
+++ beerme/src/components/Home.js
@@ -1,9 +1,24 @@
-import "../styles/app.css"
+import "../styles/app.css";
+import React, {useState, useEffect} from 'react'
 
 function Home() {
     // api function calls here maybe or in seperate folder?? Im thinking seperate folder
+    // const 
     
-
+    useEffect(() => {
+        // Fetches breweries by state and city
+        // TODO: integrate location access to allow latitude and longitude searches
+        fetch('https://api.openbrewerydb.org/breweries?by_state=ohio&by_city=columbus&per_page=10')
+        .then(response => {
+            if(response.ok) {
+                console.log(response.json())
+            }
+            throw response;
+        })
+        .catch(error => {
+            console.error("Error fetching data: ", error)
+        }, [])
+    })
     return (
         <div>
             <h1 className="home">Lets Build This App!</h1>
--- beerme/src/index.js
+++ beerme/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import App from './App';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App tab="home" />);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 
 // If you want to start measuring performance in your app, pass a function
