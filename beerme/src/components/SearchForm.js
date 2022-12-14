967384050877f96b45c04d4aab676343e6a2ac07
kdicari24@gmail.com <kdicari24@gmail.com>
2022-12-14 11:52:43 -0500

    added searchform to query api - have not added query ability to api yet

--- beerme/src/components/SearchForm.js
+++ beerme/src/components/SearchForm.js
@@ -1,11 +1,20 @@
 import React, {useState} from 'react';
 
 function SearchForm() {
+    
+    // UseState for input city search
     const [search, setSearch] = useState('');
+    
+    // Handle submit for submitting search text value 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(search)
+    }
+
     return (
-        <form onSubmit={}>
+        <form onSubmit={handleSubmit}>
             <label>Search by City:</label>
-            <input type="text" required onChange={() => setSearch(e.target.value)} />
+            <input type="text" value={search} required onChange={(e) => setSearch(e.target.value)} />
             <input type="submit" value="searchCity" />
         </form>
     )
--- beerme/src/components/SearchResultContainer.js
+++ beerme/src/components/SearchResultContainer.js
@@ -29,21 +29,3 @@ const SearchResultsContainer = () => {
 }
 
 export default SearchResultsContainer;
-
-
-
-useEffect(() => {
-    // Fetches breweries by state and city
-    // TODO: integrate location access to allow latitude and longitude searches (future development)
-    API.search
-        .then(response => {
-            if (response.ok) {
-                console.log(response.json())
-            }
-            throw response;
-        })
-        // type: cors pertains to securtiy measure from the site that hosts the fetch calls
-        .catch(error => {
-            console.error("Error fetching data: ", error)
-        }, [])
-})
